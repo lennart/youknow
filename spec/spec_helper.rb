3fcf615e54f8b978acbe635f2b52fa61cfe4b5bc
lennart <l.melzer@tu-bs.de>
2010-01-21 16:06:56 +0100

    Added simple Unit Spec and fixed duck_typed extension

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 ENV["RACK_ENV"] ||= "test"
 require ::File.join(::File.dirname(__FILE__),"..","config","boot")
+require ::File.join(::File.dirname(__FILE__),"..","config","environment")
 
 def recreate_db
   CouchRest.new(SiteConfig.couchdb_host).database!(SiteConfig.database_name).recreate!
